3cb6e9d6aae5ce4985f20e08142e12ea77455b22
michaelocana <michael@prometsource.com>
2020-11-16 22:33:17 +0800

    First commit: Drupal core 8.9.8.

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'expresso-php',
+  'username' => 'expresso-php',
+  'password' => 'expresso-php',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+ini_set('memory_limit', '512M');
